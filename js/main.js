fa34d70b1a9237f7c1eaa55c3330362954e87082
Jayandhan <jayandhanrajendran03@gmail.com>
2023-09-19 22:10:24 +0530

    cookie popup

--- js/main.js
+++ js/main.js
@@ -215,3 +215,67 @@ ghostContainer.addEventListener('mouseleave', () => {
 });
 
     
+
+
+
+
+// const cookieBox = document.querySelector(".wrapper"),
+// buttons = document.querySelectorAll(".button");
+
+// const executeCodes = () => {
+//     //if cookie contains codinglab it will be returned and below of this code will not run
+//     if(document.cookie.includes("iamjayandhan")) return;
+//     cookieBox.classList.add("show");
+
+//     // buttons.forEach(button => {
+//     //     button.addEventListener("click", () => {
+//     //         cookieBox.classList.remove("show");
+
+//     //         //if button has acceptBtn id
+//     //         if (button.id == "acceptBtn") {
+//     //         //set cookies for 1 month. 60 = 1 min, 60 = 1 hours, 24 = 1 day, 30 = 30 days
+//     //         document.cookie = "cookieBy= iamjayandhan; max-age=" + 60 * 60 * 24 * 30;
+//     //         }
+//     //     });
+//     // });
+
+
+//     buttons.forEach(button => {
+//         button.addEventListener("click", () => {
+//             cookieBox.classList.remove("show");
+    
+//             if (button.id == "acceptBtn") {
+//                 document.cookie = "cookieBy=iamjayandhan; max-age=" + 60 * 60 * 24 * 30;
+//             }
+//         });
+//     });
+// };
+// //executeCodes function will be called on webpage load
+// window.addEventListener("load", executeCodes);
+
+
+
+
+const cookieBox = document.querySelector(".wrapper");
+const buttons = document.querySelectorAll(".button");
+
+const executeCodes = () => {
+    if (document.cookie.includes("cookieBy=iamjayandhan")) return;
+
+    // Delay the display of the popup to allow the animation to work
+    setTimeout(() => {
+        cookieBox.classList.add("show");
+    }, 1000); // Adjust the delay as needed
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            cookieBox.classList.remove("show");
+
+            if (button.id == "acceptBtn") {
+                document.cookie = "cookieBy=iamjayandhan; max-age=" + 60 * 60 * 24 * 30;
+            }
+        });
+    });
+};
+
+window.addEventListener("load", executeCodes);
